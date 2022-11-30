3b9d279872813f11f19300c5c17ba48dabfa4a72
cpadridev <cpadridev@gmail.com>
2022-11-30 13:09:55 +0100

    Access to json from web

--- app/src/main/java/com/cpadridev/carmonaadrian_enterthecarapp/connection/Client.kt
+++ app/src/main/java/com/cpadridev/carmonaadrian_enterthecarapp/connection/Client.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Client {
     companion object{
-        private const val URL:String = "http://192.168.0.216:3000"
+        private const val URL:String = "https://heliotrope-dog-soccer.glitch.me/"
         var retrofit: Retrofit?= null
 
         fun getClient(): Retrofit? {
